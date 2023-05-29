00019faa802aebdef5218b343e2d1760f7cc20c0
Doridian <git@doridian.net>
2023-05-29 01:14:24 -0700

    actually calc gw v4

--- netcheck.go
+++ netcheck.go
@@ -41,32 +41,36 @@ func netcheck(id string) error {
 	log.Printf("[%s] Netcheck starting", id)
 	links, err := netlink.LinkList()
 	if err != nil {
-		return err
+		return fmt.Errorf("could not LinkList %w", err)
 	}
 
-	var routeLANv4 *netlink.Route
-	var routeDefaultv4 *netlink.Route
-	var routeDefaultULAv6 *netlink.Route
+	var routeLANv4 netlink.Route
+	var routeDefaultv4 netlink.Route
+	var routeDefaultULAv6 netlink.Route
+
+	routeLANv4.MTU = -1
+	routeDefaultv4.MTU = -1
+	routeDefaultULAv6.MTU = -1
 
 	for _, link := range links {
 		routes, err := netlink.RouteList(link, netlink.FAMILY_V4)
 		if err != nil {
-			return err
+			return fmt.Errorf("could not RouteList v4 %w", err)
 		}
 		for _, route := range routes {
 			if isLANv4Route(route) {
-				routeLANv4 = &route
+				routeLANv4 = route
 				continue
 			}
 			if isDefaultRoute(route) {
-				routeDefaultv4 = &route
+				routeDefaultv4 = route
 				continue
 			}
 		}
 
 		routes, err = netlink.RouteList(link, netlink.FAMILY_V6)
 		if err != nil {
-			return err
+			return fmt.Errorf("could not RouteList v6 %w", err)
 		}
 		for _, route := range routes {
 			if !isDefaultRoute(route) {
@@ -75,12 +79,12 @@ func netcheck(id string) error {
 			if !isULAGW(route) {
 				continue
 			}
-			routeDefaultULAv6 = &route
+			routeDefaultULAv6 = route
 		}
 	}
 
 	log.Printf("[%s] Routes: LANv4=%v Defaultv4=%v DefaultULAv6=%v", id, routeLANv4, routeDefaultv4, routeDefaultULAv6)
-	if routeLANv4 == nil {
+	if routeLANv4.MTU < 0 {
 		log.Printf("[%s] No LANv4 route found, exiting netcheck", id)
 		return nil
 	}
@@ -89,18 +93,18 @@ func netcheck(id string) error {
 	routeLANGWv4[3] = 1
 	log.Printf("[%s] GW IP: LANv4=%v", id, routeLANGWv4)
 
-	if routeDefaultULAv6 != nil {
+	if routeDefaultULAv6.MTU >= 0 {
 		log.Printf("[%s] Deleting DefaultULAv6 route %v", id, routeDefaultULAv6)
-		err = netlink.RouteDel(routeDefaultULAv6)
+		err = netlink.RouteDel(&routeDefaultULAv6)
 		if err != nil {
-			return err
+			return fmt.Errorf("could not delete DefaultULAv6 %w", err)
 		}
 	}
 
-	if routeDefaultv4 == nil || !routeLANGWv4.Equal(routeDefaultv4.Gw) {
+	if routeDefaultv4.MTU < 0 || !routeLANGWv4.Equal(routeDefaultv4.Gw) {
 		log.Printf("[%s] Changing Defaultv4 gateway to LANv4", id)
-		if routeDefaultv4 != nil {
-			err = netlink.RouteDel(routeDefaultv4)
+		if routeDefaultv4.MTU >= 0 {
+			err = netlink.RouteDel(&routeDefaultv4)
 			if err != nil {
 				return fmt.Errorf("could not delete Defaultv4 %w", err)
 			}
